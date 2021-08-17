4f1b51b9caa54dff9415236bd2c1b6a076bf2890
Hayk-21 <sakoyanhayk@gmail.com>
2021-08-17 11:33:09 +0400

    asdqwe

--- send-contact.php
+++ send-contact.php
@@ -1,24 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>XER</title>
 <?php
 require __DIR__.'/vendor/autoload.php';
+
+if(isset($_POST['phone'])) {
+  try {
 
     // Создание клиента
     $subdomain = 'hayksakoyan';            // Поддомен в амо срм
     $login     = 'hayk.sakoyan@tumo.org';            // Логин в амо срм
     $apikey    = 'a8f15a8275ea22bbdd5a86095ba5cfc2ebea5bc6';            // api ключ
 
-    try {
-        $amo = new \AmoCRM\Client($subdomain, $login, $apikey);
-        
-        // Если не указывать логин, вернутся сведения о владельце API ключа.
-        $account = $amo->account->getUserByLogin();
 
-        $unsortedId = 'a1587c45b09329a19bdf592c8a45ee37b6f095700be10f943d46820bcaee';
+    $amo = new \AmoCRM\Client($subdomain, $login, $apikey);
+    // Вывести полученые из амо данные
+    // echo '<pre>';
+    // print_r($amo->account->apiCurrent());
+    // echo '</pre>';
 
-        // Метод для принятия неразобранных заявок.
-        print_r($amo->unsorted->apiAccept($unsortedId, $account['id']));
+    // создаем лида
+    $lead = $amo->lead;
+    $lead['name'] = $_POST['product_name'];
+    // $lead['responsible_user_id'] = 2462338; // ID ответсвенного
+    // $lead['pipeline_id'] = 1207249; // ID воронки
 
-    } catch (\AmoCRM\Exception $e) {
-        printf('Error (%d): %s' . PHP_EOL, $e->getCode(), $e->getMessage());
-    }
+    // $lead->addCustomField(305117, [ // ID  поля в которое будт приходить заявки
+    //     [$_POST['city']], // сюда вписать значение из атрибута "name" пример: <input name="phone">
+    // ]);
+
+    $lead->addCustomField(917803, [
+        [$_POST['name']],
+    ]);
+
+    $lead->addCustomField(917833, [
+        [$_POST['phone']],
+    ]);
+
+    $lead->addCustomField(917835, [
+        [$_POST['email']],
+    ]);
+
+    $id = $lead->apiAdd();
+
+    // Получение экземпляра модели для работы с контактами
+    $contact = $amo->contact;
+
+    // Заполнение полей модели
+    $contact['name'] = isset($_POST['name']) ? $_POST['name'] : 'Не указано';
+    $contact['linked_leads_id'] = [(int)$id];
+
+    $contact->addCustomField(305117, [
+      [$_POST['city']],
+    ]);
+
+    $contact->addCustomField(304033, [
+      [$_POST['email'], 'PRIV'],
+    ]);
+    // Добавление нового контакта и получение его ID
+    $id = $contact->apiAdd();
+
+
+  } catch (\AmoCRM\Exception $e) {
+      printf('Error (%d): %s' . PHP_EOL, $e->getCode(), $e->getMessage());
+  }
+
+}
 
 ?>
+
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Ваша заявка успешно отправлена</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'IBM Plex Sans', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 20px;
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="title">
+            <br><span style="font-size:33px;font-weight:500;">Спасибо!</span><br><br>
+            Ваша заявка успешно отправлена.<br>
+
+            <?php if(isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER']) { ?>
+                <br><br><a href="<?php echo $_SERVER['HTTP_REFERER']; ?>" style="text-decoration: none; border-bottom: 1px dotted">Вернуться назад</a>
+             <?php } ?>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
